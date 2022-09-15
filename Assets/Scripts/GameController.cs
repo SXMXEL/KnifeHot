ad3a68b0c6ce2a820f9ff1e044471a6a4f8abb2e
Samvel Babasyan <samvel@nextepic.games>
2022-09-16 00:37:30 +0400

    Final touches

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Altom.AltUnityDriver;
 using Managers;
 using UI;
 using UnityEditor;
---
+++ Assets/Tests/Editor/PlayButtonTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using Altom.AltUnityDriver;
+using Items;
+using UnityEngine;
+
+public class PlayButtonTest
+{
+    public AltUnityDriver AltUnityDriver;
+
+    //Before any test it connects with the socket
+    [OneTimeSetUp]
+    public void SetUp()
+    {
+        AltUnityDriver = new AltUnityDriver();
+    }
+
+    //At the end of the test closes the connection with the socket
+    [OneTimeTearDown]
+    public void TearDown()
+    {
+        AltUnityDriver.Stop();
+    }
+
+    [Test]
+    public void Test()
+    {
+        //Here you can write the test
+        AltUnityDriver
+            .FindObject (By.NAME, "AltUnityRunnerPrefab")
+            .SetComponentProperty("AltUnityRunner", "ShowInputs", "true");
+        AltUnityDriver.FindObject(By.NAME, "PlayButton").Tap();
+
+        var gamePage = AltUnityDriver.WaitForObject(By.NAME, "GamePage");
+        Assert.IsTrue(gamePage.enabled);
+        AltUnityDriver.WaitForObject(By.NAME, "KnifeFireButton").Tap();
+    }
+}
